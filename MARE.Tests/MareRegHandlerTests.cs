f3f8317905f9a3504022d941b1190adc32970452
QoOdrink3r <qoodrink3r@outlook.de>
2018-10-14 09:13:50 +0200

    bugfix

--- MARE.Tests/MareRegHandlerTests.cs
+++ MARE.Tests/MareRegHandlerTests.cs
@@ -13,18 +13,19 @@ namespace MARE.Tests
         public MareRegHandlerTests()
         {
             RegKeyFake = A.Fake<IRegistryKey>();
-            A.CallTo(() => RegKeyFake.GetSubKeyNames()).Returns(new string[] { "Key" });
+            A.CallTo(() => RegKeyFake.GetSubKeyNames()).Returns(new string[] { "1" });
             A.CallTo(() => RegKeyFake.GetValue("KMD_EnableInternalLargePage", null)).Returns(0);
             A.CallTo(() => RegKeyFake.GetValue("EnableCrossFireAutoLink", null)).Returns(0);
             A.CallTo(() => RegKeyFake.GetValue("EnableUlps", null)).Returns(0);
             A.CallTo(() => RegKeyFake.GetValue("DriverDesc", null)).Returns("FAKE_GPU");
-            A.CallTo(() => RegKeyFake.SetValue(A.Dummy<string>(), A.Dummy<object>(), A.Dummy<RegistryValueKind>())).DoesNothing();
+            A.CallTo(() => RegKeyFake.SetValue(A<string>.Ignored, A<object>.Ignored, A<RegistryValueKind>.Ignored)).DoesNothing();
+            A.CallTo(() => RegKeyFake.OpenSubKey(A<string>.Ignored, A<bool>.Ignored)).Returns(RegKeyFake);
 
             RegFake = A.Fake<IRegistry>();
-            A.CallTo(() => RegFake.OpenSubKey(A.Dummy<string>())).Returns(RegKeyFake);
+            A.CallTo(() => RegFake.OpenSubKey(A<string>.Ignored)).Returns(RegKeyFake);
 
             MsgBoxFake = A.Fake<IMessageBox>();
-            A.CallTo(() => MsgBoxFake.Show(A.Dummy<string>())).DoesNothing();
+            A.CallTo(() => MsgBoxFake.Show(A<string>.Ignored)).DoesNothing();
         }
 
         [Fact]
@@ -41,7 +42,10 @@ namespace MARE.Tests
             oMareRegHandler.LoadReg();
 
             A.CallTo(() => RegFake.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}")).MustHaveHappened();
-            A.CallTo(() => RegKeyFake.GetValue(A<string>._, A<object>._)).MustHaveHappenedTwiceOrMore();
+            A.CallTo(() => RegKeyFake.GetValue("KMD_EnableInternalLargePage", null)).MustHaveHappened();
+            A.CallTo(() => RegKeyFake.GetValue("EnableCrossFireAutoLink", null)).MustHaveHappened();
+            A.CallTo(() => RegKeyFake.GetValue("EnableUlps", null)).MustHaveHappened();
+            A.CallTo(() => RegKeyFake.GetValue("DriverDesc", null)).MustHaveHappened();
             Assert.Collection(oMareRegHandler.AllGFX, x => new GFX(0, 0, 0, 0, "FAKE_GPU"));
         }
 
@@ -55,7 +59,6 @@ namespace MARE.Tests
             oMareRegHandler.Update(0, "FAKE_GPU", 1, 1, 1);
 
             Assert.Collection(oMareRegHandler.AllGFX, x => new GFX(0, 1, 1, 1, "FAKE_GPU"));
-            
         }
 
         [Fact]
